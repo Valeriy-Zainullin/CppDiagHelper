769461daa018fe1e570a81bf5a7714dff93bca6c
Valeriy Zainullin <Autaren@gmail.com>
2020-04-28 21:37:37 +0300

    На первой итерации не будем использовать "-Werror", потому что хотим иметь и предупреждения. Вообще использовать этот флаг будем, когда найдутся случаи, требующие этот флаг, при которых парсер будет работать неправильно. Флаг убран из "make_outputs.sh". Теперь "make_outputs.sh" пытается удалить собранные исполняемые файлы.

--- iterations/01/make_outputs.sh
+++ iterations/01/make_outputs.sh
@@ -4,6 +4,7 @@ for fileName in *.cpp; do
   baseName="${fileName%.*}"
   binaryName="$baseName"
   outputName="$baseName.txt"
-  LANG='C' g++ -pedantic -Wall -Wextra -Wshadow -Werror -std=c++17 "$fileName" -o "$binaryName" \
+  LANG='C' g++ -pedantic -Wall -Wextra -Wshadow -std=c++17 "$fileName" -o "$binaryName" \
   	1> "$outputName" 2>&1
+  rm "$binaryName"
 done

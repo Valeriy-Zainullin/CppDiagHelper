5d71d41134cf2bcf14e0abd0f639b14a5e0144af
Valeriy Zainullin <Autaren@gmail.com>
2020-04-28 11:41:12 +0300

    Продолжается работа над первым синтаксисом вывода компилятора.

---
+++ iterations/01_Werror/W-03.h
@@ -0,0 +1,16 @@
+#ifndef W_03_H
+#define W_03_H
+
+#include <cstdio>
+
+
+void doWork() {
+	int a = 2;
+	unsigned int b = 2;
+	if (a < b) {
+		std::printf("2 < 2u!");
+	}
+	return 0;
+}
+
+#endif
--- iterations/01_Werror/make_outputs.sh
+++ iterations/01_Werror/make_outputs.sh
@@ -4,6 +4,6 @@ for fileName in *.cpp; do
   baseName="${fileName%.*}"
   binaryName="$baseName"
   outputName="$baseName.txt"
-  LANG="C" g++ -pedantic -Wall -Wextra -Wshadow -Werror -std=c++17 "$fileName" -o "$binaryName" \
+  LANG='C' g++ -pedantic -Wall -Wextra -Wshadow -Werror -std=c++17 "$fileName" -o "$binaryName" \
   	1> "$outputName" 2>&1
 done

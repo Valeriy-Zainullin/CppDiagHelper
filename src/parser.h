63f52dd45dc774e37a92f188909279244aecd284
Valeriy Zainullin <Autaren@gmail.com>
2020-04-29 00:05:26 +0300

    Обновлён "parser.h": создан класс "Parser", который будет разбирать вывод компилятора, вызванного с локалью "C" (будем просить пользователя собирать программы с этой локалью); тип номера строки в структуре "Diagnostic" сменён на "uint32_t": этого должно быть достаточно, планируется, что мы будем ограничивать максимальный размер анализируемого вывода.

--- src/parser.h
+++ src/parser.h
@@ -1,13 +1,14 @@
 #include <cstdint>
 #include <vector>
 #include <string>
+#include <istream>
 
 
 namespace diags {
 	namespace parsing {
 		enum class DiagnosticType { ERROR, WARNING };
 		struct Diagnostic {
-			uint64_t lineNum;
+			uint32_t lineNum;
 			DiagnosticType diagType;
 			std::string description;
 
@@ -17,6 +18,36 @@ namespace diags {
 			std::string solutions;
 		};
 
-		std::vector<Diagnostic> parseFile(const char* fileName);
+		// TODO: Отказываться разбирать файлы слишком большого размера! Возможно, делать это не здесь.
+		// Разбирает вывод компилятора, вызванного с локалью "C".
+		class Parser {
+		public:
+			Parser(std::istream& inputStreamParam);
+
+			Parser& operator=(const Parser& other) = delete;
+
+			std::vector<Diagnostic> parse();
+		private:
+			static const std::size_t BUFFER_SIZE;
+			static const char END_OF_FILE;
+		private:
+			std::vector<Diagnostic> readOutput();
+			std::string readInclusionPath();
+			std::string readFilePath();
+			std::string readFunctionSignature();
+			std::string readTemplateExpansionPath();
+			uint32_t readLineNumber();
+			uint32_t readColumnNumber();
+			uint32_t readNumber();
+			std::string readDescription();
+			std::string readString();
+			std::string readConcretization();
+
+			void read();
+			void check(bool condition);
+		private:
+			char buffer[BUFFER_SIZE];
+			std::istream inputStream;
+		};
 	}
 }
